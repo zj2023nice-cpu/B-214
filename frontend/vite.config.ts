0d927fb9912a1de735881098b1aed564e6c51868
18117880068 <george_lcz@qq.com>
2026-06-08 22:15:21 +0800

    .1290141276083769:1aa7b7c82d0152d8fad5e762493d0803_6a26ca79e44b804c22c28709.6a26ca7ce44b804c22c2870b.6a26ca7cc4455036957df42d:Trae CN.T(2026/6/8 21:58:20)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,5 +16,46 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  build: {
+    target: 'es2020',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug']
+      },
+      format: {
+        comments: false
+      }
+    },
+    cssCodeSplit: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router', 'pinia'],
+          'element-plus': ['element-plus', '@element-plus/icons-vue'],
+          'echarts': ['echarts'],
+          'utils': ['axios', 'jspdf', 'html2canvas']
+        },
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const extType = info[info.length - 1]
+          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
+            return `images/[name]-[hash][extname]`
+          } else if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name)) {
+            return `fonts/[name]-[hash][extname]`
+          } else if (/\.css$/i.test(assetInfo.name)) {
+            return `css/[name]-[hash][extname]`
+          }
+          return `assets/[name]-[hash][extname]`
+        }
+      }
+    }
   }
 })
