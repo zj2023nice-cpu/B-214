dfdca3c7637fd8c194a0bcf7c2e841a581ea868e
18117880068 <george_lcz@qq.com>
2026-06-08 22:28:19 +0800

    BUG

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      algorithm: 'gzip',
+      threshold: 1024,
+      minRatio: 0.8,
+      deleteOriginFile: false
+    })
+  ],
   server: {
     host: '0.0.0.0',
     port: 3000,
